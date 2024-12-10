66e98abc3aa2384564096ac022a0e247c261d3c2
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-12-10 17:58:57 +0100

    set routers and controllers

---
+++ controllers/BookController.js
@@ -0,0 +1,2 @@
+//controllers/BookController.js
+
---
+++ routers/books.js
@@ -0,0 +1,5 @@
+//routers/books.js
+
+const express = require('express');
+const router = express.Router();
+const BookController = require('../controllers/BookController.js');
