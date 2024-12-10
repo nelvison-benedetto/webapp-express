01fff5aafdf681f42a1a4d1686b71ff0d450167f
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-12-10 18:19:12 +0100

    building sql query

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,2 +1,7 @@
 //controllers/BookController.js
 
+const fs = require('fs');  //already in node.js
+const path = require('path');  //already in node.js
+const connection = require('../db/connection.js');
+
+
--- routers/books.js
+++ routers/books.js
@@ -3,3 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const BookController = require('../controllers/BookController.js');
+
+router.get('/', BookController.index);
+router.get('/:id',BookController.show);
+router.post('/',BookController.store);
+router.put('/:id', BookController.update);
+router.delete('/:id',BookController.destroy);
+
+module.exports = router;
